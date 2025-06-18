eb26f2ca8c120146364c0e7d091188a2c9ede295
Belhanafi Abdelmadjid <112507960+BelhanafiAbdelmadjid@users.noreply.github.com>
2025-06-18 20:18:55 +0100

    refactor : project clean up

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="">
-    <h1 class="text-3xl font-montserrat  font-bold">Tets</h1>
-    <h1 class="text-3xl font-sans  font-bold">Tets</h1>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
