5dcbcbab8242eea534b0800a8d2dcc047fad5d22
Belhanafi Abdelmadjid <112507960+BelhanafiAbdelmadjid@users.noreply.github.com>
2025-06-19 20:31:58 +0100

    fix : introduction section font size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,12 +13,12 @@ import Experience from '@/components/experience/index.vue';
       <h1 class="text-xl md:text-4xl font-montserrat font-bold text-center">
         Belhanafi Abdelmadjid
       </h1>
-      <p class="text-[14px] md:text-lg font-semibold font-montserrat text-center">
+      <p class="text-[12px] md:text-lg font-semibold font-montserrat text-center">
         {{ $t("introduction") }}
       </p>
     </section>
     <section
-      class="w-screen flex flex-col items-start justify-center gap-4 px-5 py-10  text-xl md:text-2xl font-montserrat bg-beige-200 text-gray-800">
+      class="w-screen text-[16px] md:text-xl flex flex-col items-start justify-center gap-4 px-5 py-10  text-xl  font-montserrat bg-beige-200 text-gray-800">
       <span class=" font-bold">Hey!</span>
       <p class=" leading-[40px]">
         {{ $t("about.line1") }}
